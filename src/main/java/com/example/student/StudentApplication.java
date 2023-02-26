a0de481c94b5b4d0790abab6f7a4d62718cb390e
Omar Shabaan <omar.plus.077@gmail.com>
2023-02-26 22:00:09 +0200

    generate fake data

--- src/main/java/com/example/student/StudentApplication.java
+++ src/main/java/com/example/student/StudentApplication.java
@@ -1,7 +1,16 @@
 package com.example.student;
 
+import com.example.student.domain.Gender;
+import com.example.student.domain.Student;
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class StudentApplication {
@@ -10,4 +19,25 @@ public class StudentApplication {
 		SpringApplication.run(StudentApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner dataLoader(StudentRepository studentRepository) {
+		return args -> {
+			Faker faker = new Faker();
+			List<Student> students = new ArrayList<>();
+			for (int i = 0; i < 1000; i++) {
+				students.add(
+						new Student(
+							null,
+							faker.name().name(),
+							faker.internet().emailAddress(),
+							new Random().nextInt() % 2 == 0 ? Gender.MALE : Gender.FEMALE
+						)
+				);
+			}
+			studentRepository.saveAll(students);
+		};
+	}
 }
+
+
